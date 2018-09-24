598de47ed67678de012cc5c9a8df68a660ea7335
goransipic <goran.sipic87@gmail.com>
2018-09-24 22:11:27 +0200

    firebase functions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB0GJ3uraUdhJiPkO06mzJBj2pIHYDcmmU",
+    authDomain: "sefljica-6b639.firebaseapp.com",
+    databaseURL: "https://sefljica-6b639.firebaseio.com",
+    projectId: "sefljica-6b639",
+    storageBucket: "sefljica-6b639.appspot.com",
+    messagingSenderId: "262781304116"
+  }
 };
